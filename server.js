ce4995ca6f53c242d9e283cf2cc3dee930baaa29
Dinesh Prajapati <dineshprj7@gmail.com>
2026-05-25 23:45:14 +0530

    new changes

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: [
+      "https://e-commerce-website-sage-kappa.vercel.app",
+      "https://e-commerce-admin-lilac-five.vercel.app"
+    ]
+  })
+);
+
+// api endpoints
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
+app.use('/api/order',orderRouter)
+
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=> console.log('Server started on PORT : '+ port))
