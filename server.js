533522fcc61ae2a32e00a022611a906df71678b1
Dinesh Prajapati <dineshprj7@gmail.com>
2026-05-26 00:23:26 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
 
-// App Config
 const app = express()
+
 const port = process.env.PORT || 4000
+
 connectDB()
 connectCloudinary()
 
-// middlewares
 app.use(express.json())
-const cors = require("cors");
 
 app.use(
   cors({
@@ -25,16 +24,17 @@ app.use(
       "https://e-commerce-admin-lilac-five.vercel.app"
     ]
   })
-);
+)
 
-// api endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
-app.get('/',(req,res)=>{
-    res.send("API Working")
+app.get('/', (req,res)=>{
+ res.send("API Working")
 })
 
-app.listen(port, ()=> console.log('Server started on PORT : '+ port))
+app.listen(port,()=>{
+ console.log("Server started on PORT:",port)
+})
